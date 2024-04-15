c7728775a7531e2c72f54506a8ba096e96a14b33
Okashe <okashabdi88@gmail.com>
2024-04-15 10:32:41 -0500

    handling errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -169,8 +169,9 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+  
     return invoice[0];
+    
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoice.');
